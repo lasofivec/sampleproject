4cde859b77547c01fcfb9e2da515ce31f7066a19
Laura Mendoza <mendoza@math.unistra.fr>
2019-11-25 17:19:38 +0100

    now working renamed module

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='sampleproject-npyx',  # Required
+    name='npyx',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
