812c9d362728a58007270ca9410f9ecb2384f6eb
Laura Mendoza <mendoza@math.unistra.fr>
2019-11-25 16:48:42 +0100

    no need of peppercorn

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn', 'numpy', 'cython'],  # Optional
+    install_requires=['numpy', 'cython'],  # Optional
 
     cmdclass=cmdclass,
 
