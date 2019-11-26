55efb2c595418ab5e07052a020dc84dfa5022597
Laura Mendoza <mendoza@math.unistra.fr>
2019-11-26 13:41:52 +0100

    somehow still working, needs further exploration (16)

--- setup.py
+++ setup.py
@@ -40,12 +40,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    setup_requires=[
-                    'setuptools>=18.0'
-                    'cython>=0.26',
-                    'numpy',
-                    ],
-
     # This is the name of your project. The first time you publish this
     # package, this name will be registered for you. It will determine how
     # users can install this project, e.g.:
@@ -65,7 +59,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.11',  # Required
+    version='1.0.16',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
